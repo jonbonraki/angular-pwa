69cc332fc4ba6077759b9b4b5d7a2bbf0a152c8c
Michael Chavez <bigbassroller@gmail.com>
2016-09-17 12:19:25 -0700

    added services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import '../../public/css/styles.css';
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
 	selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
 	
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    // Get Heroes slowly to see how the app behaves. 
+    // Do not use in production
+    // this.heroService.getHeroesSlowly()
+    
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
+
 	onSelect(hero: Hero): void {
   	this.selectedHero = hero;
 	}
