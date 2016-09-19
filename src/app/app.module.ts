c640fc90ec4d6b30e3ba1ef9550c488d593f4864
Michael Chavez <bigbassroller@gmail.com>
2016-09-19 16:56:24 -0700

    added a dashboard navigation link to the template, just above the Heroes link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
+import { routing } from './app.routing';
 import { AppComponent }         from './app.component';
+import { DashboardComponent }         from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
