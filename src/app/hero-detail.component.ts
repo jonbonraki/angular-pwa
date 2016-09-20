1fda1a6e508966f0f6fb04cd8600723263646c51
Michael Chavez <bigbassroller@gmail.com>
2016-09-19 17:31:15 -0700

    Select a hero from the dashboard; the app should navigate directly to that hero’s details.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,34 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
+  providers: [HeroService]
 })
-export class HeroDetailComponent {
-	@Input()
-	hero: Hero;
+
+export class HeroDetailComponent implements OnInit {
+	@Input() hero: Hero;
+
+	constructor(
+	  private heroService: HeroService,
+	  private route: ActivatedRoute) {
+	}
+
+	ngOnInit(): void {
+	  this.route.params.forEach((params: Params) => {
+	    let id = +params['id'];
+	    this.heroService.getHero(id)
+	      .then(hero => this.hero = hero);
+	  });
+	}
+
+	goBack(): void {
+	  window.history.back();
+	}
 }
