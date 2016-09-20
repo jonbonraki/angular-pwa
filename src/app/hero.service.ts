d186f2053c7daaa23a89e36c893dcaca52af610c
Michael Chavez <bigbassroller@gmail.com>
2016-09-19 20:44:35 -0700

    able to save user name change

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+	private headers = new Headers({'Content-Type': 'application/json'});
   private heroesUrl = 'app/heroes';  // URL to web api
 
   constructor(private http: Http) { }
@@ -28,6 +29,15 @@ export class HeroService {
 	  return new Promise<Hero[]>(resolve =>
 	    setTimeout(resolve, 2000)) // delay 2 seconds
 	    .then(() => this.getHeroes());
+	}
+
+	update(hero: Hero): Promise<Hero> {
+	  const url = `${this.heroesUrl}/${hero.id}`;
+	  return this.http
+	    .put(url, JSON.stringify(hero), {headers: this.headers})
+	    .toPromise()
+	    .then(() => hero)
+	    .catch(this.handleError);
 	}
 
 	private handleError(error: any): Promise<any> {
